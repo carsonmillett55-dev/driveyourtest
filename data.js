3aa55b4cf435da5ffe74d44758824e119db0829a
carsonmillett55-dev <carsonmillett55@gmail.com>
2026-02-20 15:48:17 -0800

    Create data.js with required data arrays

---
+++ data.js
@@ -0,0 +1,5 @@
+const permitFlashcards = [/* Your data here */];
+const permitTestQuestions = [/* Your data here */];
+const scenarioQuestions = [/* Your data here */];
+
+export { permitFlashcards, permitTestQuestions, scenarioQuestions };
